076b90608cdf5bf3a4cabf7fe527ff743f668e2c
Beatrice D’Anna <beatricedanna@MacBook-di-Beatrice.local>
2025-06-25 12:16:07 +0200

    2nd function uploaded

---
+++ R/ExtractData.R
@@ -0,0 +1,37 @@
+# OKAY
+# To execute the function:
+# results <- ExtractData(combined_dt)
+
+# Recall gene_expression_dt and atac_seq_dt:
+# gene_expression_dt <- results$gene_expression
+# atac_seq_dt <- results$atac_seq
+
+# Check:
+# print(gene_expression_dt[1:5, 1:5])
+# print(atac_seq_dt[1:5, 1:5])
+
+# Declare 'feature_id' as a global variable to avoid NOTES during R CMD check
+# This is necessary because 'feature_id' is used as a column name inside data.table expressions
+utils::globalVariables("feature_id")
+
+#' ExtractData
+#'
+#' @importFrom data.table %like%
+#' @param combined_dt A combined data table containing feature_id, cell_barcodes and values of both peaks and gene expression.
+#'
+#' @return Two separate data table, one for gene expression data and one for atac seq data.
+#'
+#' @export
+ExtractData <- function (combined_dt){
+  # Subset creation
+  gene_expression_dt <- combined_dt[feature_id %like% "^ENSG"] # Assign to a subset (gene_expression_dt) all values of feature_id that start with ENSG
+  atac_seq_dt <- combined_dt[feature_id %like% "^chr"]  # Assign to a subset (atac_seq_dt) all values of feature_id that start with chr
+
+  cat("Gene expression data:", nrow(gene_expression_dt), "genes x", ncol(gene_expression_dt)-1, "cells\n") #ncol-1 because one column is for the ids
+  cat("ATAC-seq data:", nrow(atac_seq_dt), "peaks x", ncol(atac_seq_dt)-1, "cells\n") #ncol-1 because one column is for the ids
+
+  return(list(
+    gene_expression_dt,
+    atac_seq_dt
+  ))
+}
--- R/SparseToDataTable.R
+++ R/SparseToDataTable.R
@@ -1,14 +1,60 @@
+# OKAY
+# Before running the function:
+
+# Load the libraries
+# library(data.table)
+# library(Matrix)
+
+# Set working directory
+# setwd("~/Desktop/project")
+
+# Load the files
+# matrix_file   <- "matrix.mtx.gz"
+# features_file <- "features.tsv.gz"
+# barcodes_file <- "barcodes.tsv.gz"
+
+# To execute the function:
+# combined_dt <- SparseToDataTable(mtx)
+
+# Read the files:
+# mtx <- readMM(file = matrix_file)
+# features <- fread(features_file, header = FALSE)
+# barcodes <- fread(barcodes_file, header = FALSE)
+
+# Check of the dimensions:
+# cat("Sparse matrix dimension:", dim(mtx)[1], "features x", dim(mtx)[2], "barcodes\n")
+
+# To execute the function:
+# source("~/Desktop/functions/SparseToDataTable.R")
+# combined_dt <- SparseToDataTable(mtx, features, barcodes)
+
 #' SparseToDataTable
+#'
 #' @importFrom data.table as.data.table
 #' @importFrom utils head
 #' @param mtx A sparse matrix (e.g., class dgTMatrix or dgCMatrix).
+#' @param features Gene ids or chromosome ids.
+#' @param barcodes Cell barcodes for identification.
 #'
 #' @return A data.table with columns: feature_id, cell_barcode, value.
 #'
 #' @export
-SparseToDataTable <- function(mtx){
+#'
+
+SparseToDataTable <- function (mtx, features, barcodes){
+  if (nrow(mtx) == nrow(barcodes)) {
+    cat("Matrix found with the wrong orientation -> Transposition in progres...\n")
+    mtx <- t(mtx)
+  }
+  # Assign rownames and colnames
+  rownames(mtx) <- features$V1
+  colnames(mtx) <- barcodes$V1
+
+  # Conversion in dense matrix
   full_mat <- as.matrix(mtx)
-  data_table <- as.data.table(full_mat,keep.rownames = "row.names")
-  print(head(data_table))
-  return(data_table)
+
+  # Conversion in data.table with row names assigned as feature_id
+  combined_dt <- as.data.table(full_mat, keep.rownames = "feature_id")
+
+  return(combined_dt)
 }
